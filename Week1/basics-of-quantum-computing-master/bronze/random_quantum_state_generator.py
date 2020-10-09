0545dcec527a3b5890907276a57c70496a23b948
nopantsmonkey <aamir.shah.92@gmail.com>
2020-10-10 02:46:39 +0500

    finished quantum tomography

---
+++ Week1/basics-of-quantum-computing-master/bronze/random_quantum_state_generator.py
@@ -0,0 +1,10 @@
+# your function is here
+from math import cos, sin, pi
+from random import randrange
+def random_quantum_state():
+    angle = randrange(360)
+    radians = 2*pi*myangle/360
+    x = cos(radians)
+    y = sin(radians)
+    
+    return [x, y]
